13ad013d70355662080cf4cbacbcfccb9ec2706f
kerinhanabi <kerinhanabi@hotmail.com>
2024-08-25 23:37:28 +0800

    commit message

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,44 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <captioned-content>
+  <template v-slot:content> <!-- a normal format to write v-slot -->
+
+  </template>
+  <template v-slot:caption> 
+    
+  </template>
+ </captioned-content>
+
+ <captioned-content>
+  <template #content> <!-- short-handed format to write v-slot -->
+    <img src="./assets/img_apple.svg" alt="">
+  </template>
+  <template #caption="{ source }"> <!-- scoped slot from original <template #caption> -->
+    source : <a href="samplesource.co">{{ source }}</a>
+  </template>
+ </captioned-content>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SlotCaptionCcontent from './components/SlotCaptionContent.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+export default{
+  components:{
+    SlotCaptionCcontent,
+  },
+  // scoped slot : move the below code from parent App.vue move the below code to child component SlotCaptionContent.vue 
+  // data() {
+  //   return {
+  //     source: "samplesource.co",
+  //   }
+  // }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  width: 100%;
+  margin: 0;
+  padding: 0;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/SlotCaptionContent.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <slot name="content">
+            <img src="../assets/img_fish.svg" alt="">
+        </slot>
+        <p>
+            <slot name="caption" v-bind:source="source">Caption</slot> <!-- scoped slot : original <slot name="caption"> -->
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+// scoped slot : below code moved from app.vue to here
+data() {
+    return {
+      source: "samplesource.co",
+    }
+  }
+}
+</script>
+
+<style scoped>
+img {width: 100%;}
+</style>
